d22a2acd3593376a09b854f09e96eaca75189f2d
Thomah6 <agapethounk@gmail.com>
2025-11-21 17:32:01 +0100

    feat: auth and roles setup - Install Laravel Breeze -Add avatar column to users table 6Install Spatie Permission package -Configure user model with HasRoles -Seed database with Admin and User roles

---
+++ database/migrations/2025_11_21_154320_add_avatar_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //On ajoute la colonne avatar après l'email qui peut etre null
+            $table->string('avatar')->nullable()->after('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
